7ddb98d48f657b3fefb1005b43ccd361943bb98b
Dhiraj <sharmadheeraj849@gmail.com>
2022-05-05 10:31:20 +0530

    re

--- routes/cart.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-var cartController = require('../app/controller/cartController')
-
-
-
-router.get('/cart',cartController.index);
-router.post('/updatecart',cartController.update);
